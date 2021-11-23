7dec367f4adb9c045d5832d0573ed63466558523
MossCoffee <Moss6sOfficial@gmail.com>
2021-11-22 23:54:34 -0700

    Updated import window title

--- Source/VivSpriteImporter/Private/VivSpriteImporter.cpp
+++ Source/VivSpriteImporter/Private/VivSpriteImporter.cpp
@@ -54,7 +54,7 @@ void FVivSpriteImporterModule::PluginButtonClicked()
 {
 	FString LastFilePath = FString("C:\\");
 	FString FilePath;
-	if(OpenFile(FString("Select Asset"), FString(".vivsprite"), LastFilePath, FilePath)){
+	if(OpenFile(FString("Select Vivsprite to import"), FString(".vivsprite"), LastFilePath, FilePath)){
 		VivSpriteParser parser(FilePath);
 	}
 
